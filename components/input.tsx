a2073b7d6a3d9f968b513ac8f590e3a92c8d7643
Alozie Miracle Patrick <nacotech09@gmail.com>
2025-11-25 13:33:28 +0100

    removed word tokenizer

--- components/input.tsx
+++ components/input.tsx
@@ -11,28 +11,45 @@ interface Data {
   part_of_speech: string
 }
 
+interface WordFromBackend {
+  original_text: string,
+  results: Data[]
+}
+
 const Input = () => {
   const [word, setWord] = useState("")
-  const [data, setData] = useState<Data>()
+  const [data, setData] = useState<WordFromBackend>()
+  const [loading, setLoading] = useState(false)
 
   const submit = () => {
       if (!word) return alert("Please send in an english word");
+
+      setLoading(true)
 
       axios.post("https://root-word-originator-ml-backend.vercel.app/root", {
         word
       }).then(res => {
         console.log(res.data);
         setData(res.data)
+      }).catch(err => {
+        console.log(err);
+      }).finally(() => {
+        setLoading(false)
       })
   }
 
   const clickedWord = (text: string) => {
     setWord(text)
+    setLoading(true)
     axios.post("https://root-word-originator-ml-backend.vercel.app/root", {
         word: text
       }).then(res => {
         console.log(res.data);
         setData(res.data)
+      }).catch(err => {
+        console.log(err);
+      }).finally(() => {
+        setLoading(false)
       })
   }
     
@@ -40,17 +57,17 @@ const Input = () => {
     <>
         <ul className="flex gap-5">
             {badges.map((word) => (
-              <button onClick={() => clickedWord(word.name)} key={word.id} className="border p-1 rounded-full px-5 cursor-pointer border-gray-400 transition-all duration-300 ease-in-out active:scale-95">{word.icon} {word.name}</button>
+              <button disabled={loading} onClick={() => clickedWord(word.name)} key={word.id} className="border p-1 rounded-full px-5 cursor-pointer border-gray-400 transition-all duration-300 ease-in-out active:scale-95">{word.icon} {word.name}</button>
             ))}
           </ul>
       <div className="max-w-3xl w-full border rounded-2xl p-5 mt-5 shadow-2xl border-[#582aff] flex items-center gap-3">
         <MagnifyingGlassIcon className="h-5 w-5 text-[#582aff]" />
         <input type="text" value={word} onChange={e => setWord(e.target.value) } className="w-full bg-transparent outline-none border-none text-[#582aff]" placeholder="Search any English words to get its root word" />
-        <PaperAirplaneIcon onClick={submit} className=" h-5 w-5 cursor-pointer transition-all duration-300 ease-in-out active:scale-95 hover:text-[#582aff] rotate-[-90deg] hover:rotate-0 " />
+        <button onClick={submit} disabled={loading}><PaperAirplaneIcon  className=" h-5 w-5 cursor-pointer transition-all duration-300 ease-in-out active:scale-95 hover:text-[#582aff] rotate-[-90deg] hover:rotate-0 " /></button>
       </div>
       <div className="max-w-2xl w-full flex flex-col gap-5">
-        <p className="text-sm italic text-gray-500 text-center">Your root word will appear here... <span className="text-[#582aff]">{data?.root_word ? data.root_word.toUpperCase() : "ROOT WORD"}</span></p>
-        <p className="text-sm italic text-gray-500 text-center">Part of Speech of {word}: <span className="text-[#582aff]">{data?.part_of_speech ? data.part_of_speech : "PART OF SPEECH"}</span></p>
+        <p className="text-sm italic text-gray-500 text-center">Your root word will appear here... <span className="text-[#582aff]">{data && data.results.map(r => r.root_word).join(", ")}</span></p>
+        <p className="text-sm italic text-gray-500 text-center">Part of Speech of {word}: <span className="text-[#582aff]">{data && data.results.map(r => r.part_of_speech).join(", ")}</span></p>
       </div>
     </>
   )
